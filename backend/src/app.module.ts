f1f33cdd3d7734b2ce52b8628786114d532a5d1e
jishiHashtag <jishi@hashtagtechno.com>
2025-10-01 13:20:42 +0400

    Updated TypeORM config for Neon database

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,6 +12,17 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
+      host: 'ep-shiny-heart-ad9yq40x-pooler.c-2.us-east-1.aws.neon.tech',
+      port: 5432,
+      username: 'neondb_owner',
+      password: 'npg_tSDbI6XMRk4a',
+      database: 'neondb',
+      ssl: { rejectUnauthorized: false },
+      entities: [User, Subscriber],
+      synchronize: true,
+    }),
+    /* TypeOrmModule.forRoot({
+      type: 'postgres',
       url: process.env.DATABASE_URL,
       host: 'localhost',
       port: 5432,
@@ -20,7 +31,7 @@ import { ConfigModule } from '@nestjs/config';
       database: 'taskdb',
       entities: [User, Subscriber],
       synchronize: true,
-    }),
+    }),*/
     TypeOrmModule.forFeature([User]), // optional here, handled in feature modules
     UsersModule,
     AuthModule,
